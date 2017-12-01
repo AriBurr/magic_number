82c667a36bbd5aa643c4d9c2ac17027f40322b29
Ariel Burriss <ariburr@gmail.com>
2017-12-01 10:43:47 -0700

    error checking for negative and non-numbers

--- magic_number.rb
+++ magic_number.rb
@@ -2,15 +2,16 @@ require 'to_words'
 
 def magic_number
 
-  puts "Input any number or type quit:"
-  num = gets.strip.to_i
+  puts "Input any number:"
+  num = gets.strip
+  num =~ /\d/ ? check_magic(num) : magic_number
 
-  check_magic(num)
   play_again
 
 end
 
 def check_magic(num)
+  if num.to_i >= 0
     puts "#{num} is #{num.to_words.length}"
     output = num.to_words.length
     if output == 4
@@ -18,13 +19,15 @@ def check_magic(num)
     else
       check_magic(output)
     end
+  else
+    puts "Please input positive number."
+    magic_number
+  end
 end
 
 def play_again
   puts "Would you like see more magic?\n[1] Yes [2] No"
-
   gets.strip.to_i == 2 ? exit(0) : magic_number
-
 end
 
 magic_number
